8f9e97a4662d50e1f1b5875ed7f503611067e146
Adarsh Kumar <131886833+Adarshlpu@users.noreply.github.com>
2026-05-22 14:39:16 +0530

    Update API base URL to use environment variable

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -2,7 +2,9 @@ import axios from "axios";
 
 const API = axios.create({
    // baseURL: "http://localhost:5000/api/auth",
-    baseURL:"https://naintaara-backend.onrender.com/api/auth",
+    // baseURL:"https://naintaara-backend.onrender.com/api/auth",
+   baseURL:
+`${import.meta.env.VITE_API_URL}/auth`
 });
 
 
